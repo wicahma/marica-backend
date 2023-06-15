92b9ded67597ffe2cdc28bc121d440067b7d7399
wicahma <teguh.dc.kusuma@gmail.com>
2023-06-15 15:46:01 +0700

    update: image update route

--- src/routes/video.js
+++ src/routes/video.js
@@ -5,8 +5,10 @@ const {
   updateVideo,
   likeVideo,
   getAllVideo,
+  updateImageVideo,
 } = require("../controllers/video");
 const { authJWT } = require("../middlewares/auth");
+const { thumbnailVideoHandler } = require("../middlewares/multer");
 const { sessionChecker } = require("../middlewares/session-checker");
 
 const router = require("express").Router();
@@ -23,6 +25,7 @@ router
     authJWT,
     (req, res, next) =>
       sessionChecker(req, res, next, { admin: true, validated: true }),
+    thumbnailVideoHandler.single("thumbnail"),
     createVideo
   );
 
@@ -49,6 +52,14 @@ router
       sessionChecker(req, res, next, { admin: true, validated: true }),
     deleteVideo
   );
+
+router.route("/image/:id").put(
+  authJWT,
+  (req, res, next) =>
+    sessionChecker(req, res, next, { admin: true, validated: true }),
+  thumbnailVideoHandler.single("thumbnail"),
+  updateImageVideo
+);
 
 router
   .route("/:id/like")
