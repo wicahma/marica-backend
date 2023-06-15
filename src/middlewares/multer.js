d0a45b8cd7310faa3d20302dd002f3e612a7a09b
wicahma <teguh.dc.kusuma@gmail.com>
2023-06-15 15:46:26 +0700

    update: fixing file handler for  image file

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,22 +1,62 @@
-const Multer = require("multer");
+const multer = require("multer");
+const fs = require("fs");
 
-const multer = Multer({
-  storage: Multer.diskStorage({
-    destination: function (req, file, callback) {
-      callback(null, `${__dirname}../../tmp`);
+const thumbnailVideoHandler = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      if (!fs.existsSync(`${__dirname}/../../public/images`)) {
+        fs.mkdirSync(`${__dirname}/../../public/images`, { recursive: true });
+      }
+      cb(null, `${__dirname}/../../public/images`);
     },
-    filename: function (req, file, callback) {
-      callback(
-        null,
-        file.fieldname + "_" + Date.now() + "_" + file.originalname
-      );
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + "_VIDEO_THUMBNAIL_" + file.originalname);
     },
   }),
-  limits: {
-    fileSize: 5 * 5_000_000, 
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.includes("image")) {
+      cb(null, true);
+    } else {
+      cb(new Error("File harus berupa gambar!"), false);
+    }
   },
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
-module.exports = {
-  multer,
+const thumbnailSeriesHandler = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      if (!fs.existsSync(`${__dirname}/../../public/images`)) {
+        fs.mkdirSync(`${__dirname}/../../public/images`, { recursive: true });
+      }
+      cb(null, `${__dirname}/../../public/images`);
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + "_SERIES_THUMBNAIL_" + file.originalname);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.includes("image")) {
+      cb(null, true);
+    } else {
+      cb(new Error("File harus berupa gambar!"), false);
+    }
+  },
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+const deleteFile = (filePath) => {
+  if (!fs.existsSync(filePath))
+    return "Image File not found, but data was deleted!";
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.log(err);
+      return `Internal error when deleting file: ${err}`;
+    } else {
+      console.log("File deleted");
+      return "Image File deleted succesfully";
+    }
+  });
 };
+
+module.exports = { thumbnailVideoHandler, thumbnailSeriesHandler, deleteFile };
